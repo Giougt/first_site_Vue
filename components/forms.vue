dda54ead9ee93c0b179b72d3e20ae9e31e26ca93
Giougt <154607600+Giougt@users.noreply.github.com>
2024-10-27 02:05:42 +0200

    Create forms.vue

---
+++ components/forms.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
+<style>
+  button{
+    background: red;
+    padding : 10px 40px 30px 50px;
+  }
+</style>
