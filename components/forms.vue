a4b91f5080d26bb530a68501d697582c2575eabb
Giougt <154607600+Giougt@users.noreply.github.com>
2024-11-02 01:46:59 +0100

    Update forms.vue

--- components/forms.vue
+++ components/forms.vue
@@ -1,15 +1,7 @@
 <script setup>
 import { ref } from 'vue'
-
-const count = ref(0)
 </script>
 
 <template>
-  <button @click="count++">You clicked me {{ count }} times.</button>
+<div class="title"> Title </div>
 </template>
-<style>
-  button{
-    background: red;
-    padding : 10px 40px 30px 50px;
-  }
-</style>
